1e738669ee9dfa8e259d9151b551e123bca08a94
qitas <qitas@qitas.cn>
2019-03-27 10:32:38 +0800

    fix somefiles

--- linux/kernel/include/generated/compile.h
+++ linux/kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 PREEMPT Wed Mar 13 11:40:24 CST 2019"
+#define UTS_VERSION "#2 PREEMPT Wed Mar 27 10:23:10 CST 2019"
 #define LINUX_COMPILE_BY "qitas"
 #define LINUX_COMPILE_HOST "gateway"
 #define LINUX_COMPILER "gcc version 4.6.3 20120201 (prerelease) (crosstool-NG linaro-1.13.1-2012.02-20120222 - Linaro GCC 2012.02) "
